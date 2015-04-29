894a8c75038d0b689fb6864af7dfe257026d28b5
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-28 18:30:59 -0700

    Initial commit

---
+++ client/config/config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('eTrade')
+.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {url: '/', templateUrl: '/views/general/home.html'})
+  .state('register', {url: '/register', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
+  .state('login', {url: '/login', templateUrl: '/views/users/users.html', controller: 'UsersCtrl'})
+  .state('profile', {url: '/profile', templateUrl: '/views/profile/profile.html', controller: 'ProfileCtrl'})
+  .state('portfolios', {url: '/portfolios', templateUrl: '/views/portfolios/portfolios.html', abstract: true})
+  .state('portfolios.list', {url: '', templateUrl: '/views/portfolios/portfolios-list.html', controller: 'PortfoliosListCtrl'})
+  .state('portfolios.new', {url: '/new', templateUrl: '/views/portfolios/portfolios-new.html', controller: 'PortfoliosNewCtrl'})
+  .state('portfolios.show', {url: '/{name}', templateUrl: '/views/portfolios/portfolios-show.html', controller: 'PortfoliosShowCtrl'});
+});
---
+++ client/config/constants.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('eTrade')
+.constant('firebaseUrl', 'https://newstockx.firebaseio.com/');
